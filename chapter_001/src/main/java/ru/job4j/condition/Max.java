7f62f5c75d312d7979926fb5f381d6818ac37e91
Vasiliy Kozlov <program1408@gmail.com>
2019-09-13 10:13:53 +0300

    Максимум из двух чисел [#175527]_2

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -2,13 +2,6 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        boolean condition = true;
-        if (left > right) {
-            condition = true;
-        } else {
-            condition = false;
-        }
-        int result = condition ? left : right;
-        return result;
+        return left > right ? left : right;
     }
 }
