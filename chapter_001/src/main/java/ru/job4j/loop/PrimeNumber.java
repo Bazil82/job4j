8f5a4ca20471ce2de3b5304d402dfb401ab39c54
Vasiliy Kozlov <program1408@gmail.com>
2019-09-20 11:36:13 +0300

    Простые числа. [#175541]_2

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -3,8 +3,8 @@ package ru.job4j.loop;
 public class PrimeNumber {
     public int calc(int finish) {
         int count  = 0;
+        CheckPrimeNumber num = new CheckPrimeNumber();
         for (int i = 2; i <= finish; i++) {
-            CheckPrimeNumber num = new CheckPrimeNumber();
             if (num.check(i) == true) {
                 count++;
             }
