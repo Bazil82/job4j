3e078246679ccce451bc84b29ee360e13f74fb9e
Vasiliy Kozlov <program1408@gmail.com>
2019-10-03 12:35:20 +0300

    Слоо заканчивается на... [#156317]

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsW(char[] world, char[] post) {
+        boolean res = true;
+        for (int i = 0; i < post.length; i++) {
+            if (world[world.length - i - 1] != post[post.length - i - 1]) {
+                res = false;
+            }
+        }
+        return res;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/EndsWithTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class EndsWithTest {
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'o'};
+        boolean res = EndsWith.endsW(word, post);
+        assertThat(res, is(true));
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'a'};
+        boolean res = EndsWith.endsW(word, post);
+        assertThat(res, is(false));
+    }
+}
