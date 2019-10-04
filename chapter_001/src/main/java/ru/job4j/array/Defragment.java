db17cb40f2b4e13189a645d02e6657d5eeaa403c
Vasiliy Kozlov <program1408@gmail.com>
2019-10-04 15:33:43 +0300

    Дефрагментация массива. [#225]

--- chapter_001/src/main/java/ru/job4j/array/Defragment.java
+++ chapter_001/src/main/java/ru/job4j/array/Defragment.java
@@ -2,10 +2,14 @@ package ru.job4j.array;
 
 public class Defragment {
     public static String[] compress(String[] array) {
+        String temp;
+        int count = 0;
         for (int i = 0; i < array.length; i++) {
-            String cell = array[i];
-            if (cell == null) {
-
+            if (array[i] != null) {
+                temp = array[count];
+                array[count] = array[i];
+                array[i] = temp;
+                count++;
             }
         }
         return array;
